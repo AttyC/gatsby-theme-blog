ae5b7c110e8fa51fd3b3f5f21ef4ba3d4473d746
CSA Web Developer <antoniajcronin@gmail.com>
2020-02-17 12:13:00 +0000

    commit all files

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `My Blog Title`,
+    author: `Atty Ward`,
+    description: `Atty Ward's Gatsby Theme Blog`  ,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/gatsbyjs`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/gatsbyjs`,
+      },
+    ],
+  },
+}
